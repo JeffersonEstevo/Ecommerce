f8fdd838a64b6dcc9f567e2ff9a33b40baae6b79
JeffersonEstevo <jefferson.estevo@gmail.com>
2025-02-03 00:36:44 -0300

    Atualizando settings.py e urls.py

--- loja/settings.py
+++ loja/settings.py
@@ -66,7 +66,9 @@ ROOT_URLCONF = 'loja.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates') 
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,9 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join('static')
+    os.path.join('templates/static')
 ]
 
 MEDIA_URL = '/media/'
--- loja/urls.py
+++ loja/urls.py
@@ -21,9 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
-]
+      
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # TODO: Remover debug toolbar
 if settings.DEBUG:
